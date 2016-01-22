76963a2b2760bc8ac6002227df8d02d415cfa0e0
Saria Islam <saraaaay@gmail.com>
2016-01-21 21:53:58 -0800

    Add method to remove entries from address book

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,26 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.size).to eq(0)
+    end
+
+    it "removes the entry with the correct information" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Saria Islam', '410.615.9468', 'saraaaay@gmail.com')
+
+      removed_entry = book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(removed_entry.name).to eq('Ada Lovelace')
+      expect(removed_entry.phone_number).to eq('010.012.1815')
+      expect(removed_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
 end
